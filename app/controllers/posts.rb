67aa16affd9228fd5c9581a69ec4ddcb30074aa8
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-13 17:47:03 -0500

    added

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -4,25 +4,25 @@ get '/posts' do
   erb :posts
 end
 
-post '/posts' do
-  puts params.inspect
-end
-
 get '/posts/:id' do
   @post = Post.find(params[:id])
   erb :post_show
 end
 
 get '/post/new' do
-  @post = Post.new(params[:post])
   erb :post_new
 end
 
-post '/posts' do
-  @post = Post.new(params[:post])
-  if @post.save 
-    redirect to('/')
+post '/post/new' do
+  @post = Post.create(params[:post])
+  if @post.valid? 
+    redirect to('/posts')
   else
     erb :post_new
   end
+end
+
+post '/posts' do
+  p @posts = Post.all
+  erb :posts
 end
